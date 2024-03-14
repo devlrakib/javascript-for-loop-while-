84b21d705a770257d67c703e55c9cf5303a4a870
devlrakib <devrakib5@gmail.com>
2024-03-14 20:26:56 +0600

    first commit

---
+++ for.js
@@ -0,0 +1,15 @@
+// !While Loop Javascript Code 
+// var roastgiven = 0;
+// while (roastgiven < 7){
+//     console.log("Hello Javascript");
+//    roastgiven++;
+// }
+// !For Loop Javascript Code
+// for ( var roastgiven=0; roastgiven < 7; roastgiven++){
+//    console.log('Hello Javascript');
+// }
+
+// !Simple Version Of For Loop 
+for (var i=0; i<4; i++){
+    console.log('This is javascript Code');
+}
